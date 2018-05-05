9e11308533fb94f57661fe5e57b6eda9e13415e1
risexie <rise_xie@sina.com>
2018-05-05 16:21:17 +0800

    增加主页面样式

--- route.api.js
+++ route.api.js
@@ -2,13 +2,8 @@ var express = require('express');
 var router = express.Router();
 var PostModel = require('./models/post');
 
-  /* GET users listing. */
-  router.get('/users', function(req, res, next) {
-    res.send('respond with a resource');
-  });
-
 /* Get lists data*/ 
-router.get('/list',function(req, res, next){
+router.get('/posts/list',function(req, res, next){
   PostModel.find({},{},function(err,posts){
     if(err){
       res.json("返回数据失败");
@@ -18,7 +13,7 @@ router.get('/list',function(req, res, next){
   })
   
 });
-router.post('/list/create',function(req, res, next){
+router.post('/post/create',function(req, res, next){
   var title = req.body.title;
   var content = req.body.content;
 
--- route.page.js
+++ route.page.js
@@ -1,29 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var marked = require('marked');
 var PostModel = require('./models/post');
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', {
-    title: 'Express'
+    title: 'Home page'
   });
 });
 
-/* GET create listing. */
-router.get('/create', function (req, res, next) {
+/* GET create page. */
+router.get('/posts/create', function (req, res, next) {
   res.render('create', {
     title: 'create'
   });
 });
 
-/* GET post content*/
-router.get('/show',function(req,res,next){
-var id = req.query.id;
-
-PostModel.findById(id,function(err,show){
-  if(err){
-    console.log(err)
-  }
-  res.render('show',{show})
+/* GET post listing*/
+router.get('/posts', function (req, res, next) {
+  res.render('posts', {
+    title: "post"
+  });
 });
+
+/* GET show content*/
+router.get('/posts/show', function (req, res, next) {
+  var id = req.query.id;
+
+  PostModel.findById(id, function (err, show) {
+    if (err) {
+      console.log(err)
+    }
+    show.content = marked(show.content)
+    res.render('show', {
+      show
+    })
+  });
 });
 module.exports = router;
